a0a39d899f66d1b99567de81c85de6e1b1b14a32
Abhishek Sanjay Gawade <supeduperr.oa@gmail.com>
2021-04-14 16:58:45 +0530

    fornt end of item add page is  completed
    See :#10

--- easylife_admin/urls.py
+++ easylife_admin/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.conf.urls.static import static 
 from django.conf import settings 
 from .views import (
-                SuccessView,All_product_list
+                SuccessView,All_product_list,ItemCreateView
                     )
 
 app_name='easylife_admin'
@@ -10,6 +10,7 @@ app_name='easylife_admin'
 
 urlpatterns = [
 
+	 path('add_items/', ItemCreateView.as_view(), name='add_items'),
 	 path('all_items/', All_product_list.as_view(), name='all_items'),
     path('success/', SuccessView.as_view(), name='success'),
 
--- easylife_admin/views.py
+++ easylife_admin/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-from django.views.generic import ListView,DetailView,View
+from django.views.generic import ListView,DetailView,View,CreateView
 from base.models import Item,Order,OrderItem,BillingAddress,Comment
 
 
@@ -15,6 +15,74 @@ class All_product_list(ListView):
     model = Item
     context_object_name = 'items'
     template_name='easylife_admin/all_items.html'
+
+
+
+
+class ItemCreateView(CreateView):
+    model = Item
+
+
+
+
+
+
+
+
+
+
+    fields =('title',
+				'price',
+				'discount_price',
+				'category',
+				'label_name',
+				'label',
+				'description',
+				'image')
+    template_name = "easylife_admin/create_new_item.html"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# @login_required
+# def NoteUpdateView(request,pk):
+# 	obj = get_object_or_404(Note, id = pk)
+
+# 	form = NoteForm(request.POST or None, instance = obj)
+
+# 	if form.is_valid(): #send form
+# 		cd=form.cleaned_data
+# 		if cd.get('password_required') or  (obj.password_required):
+# 			item_dict['update_the_note']=[form,pk]
+# 			request.session['allow_if_password_is_confirm']=False
+# 			return redirect('check_password')
+# 		else:
+# 			form.save()
+# 			messages.success(request, f'''Note <a href="{reverse('note_update',args=[str(obj.pk)])}" class="alert-link">"{obj.heading}"</a> successfully Updated''')
+# 			return redirect(Note.objects.filter(author=request.user).get(id=pk).get_absolute_url())
+
+# 	else:#want form
+# 		if (not obj.password_required) or request.session.get('allow_if_password_is_confirm'):
+# 			return render(request,'noteshtmls/note_update.html',{'form':form,'object':obj})	
+# 		elif (obj.password_required):
+# 			request.session['allow_if_password_is_confirm']=False
+# 			item_dict['allow_update_form']=obj.pk
+# 			return redirect('check_password')
+
+
 
 
 
